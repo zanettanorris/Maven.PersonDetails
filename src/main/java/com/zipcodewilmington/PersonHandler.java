44ec8685ab30fb4467b45278abbfc4752511e2ef
Zanetta Norris <znorris@zipcodes-MacBook-Pro-2.local>
2020-02-23 23:45:49 -0500

    Completed for while and each loops, fixed test return type. All tests pass.

--- src/main/java/com/zipcodewilmington/PersonHandler.java
+++ src/main/java/com/zipcodewilmington/PersonHandler.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington;
 
+import java.lang.reflect.Array;
+
 /**
  * Created by leon on 1/24/18.
  */
@@ -12,49 +14,31 @@ public class PersonHandler {
 
     public String whileLoop() {
         String result = "";
-        // create a `counter`
-        // while `counter` is less than length of array
-            // begin loop
-
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-
-            // end loop
+        int counter = 0;
+        while (counter < personArray.length) {
+            String currentPerson = personArray[counter].toString();
+            result += currentPerson;
+            counter++;
+        }
         return result;
     }
-
-
 
     public String forLoop() {
         String result = "";
-        // identify initial value
-        // identify terminal condition
-        // identify increment
-
-        // use the above clauses to declare for-loop signature
-            // begin loop
-                // use `counter` to identify the `current Person` in the array
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
-
+        for (int i=0; i < personArray.length;){
+            String currentPerson = personArray[i].toString();
+            result += currentPerson;
+            i++;}
         return result;
     }
-
 
 
     public String forEachLoop() {
         String result = "";
-        // identify array's type
-        // identify array's variable-name
-
-        // use the above discoveries to declare for-each-loop signature
-            // begin loop
-                // get `string Representation` of `currentPerson`
-                // append `stringRepresentation` to `result` variable
-            // end loop
-
+        for (Person i : personArray) {
+            String currentPerson = i.toString();
+            result += currentPerson;
+        }
         return result;
     }
 
